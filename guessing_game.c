6a4a274b8c81fcccff939a2f378ee8b2c1c8b16b
Taaha Bhatti <TBhatti90@gmail.com>
2022-12-22 15:41:57 -0500

    Limited the number of guesses and limited the range of the values from 1 to 10

--- guessing_game.c
+++ guessing_game.c
@@ -7,6 +7,7 @@ int main()
 {
 	int secret;
 	int guess;
+	int countdown;
 	int c;
 	char buf[20];
 	char input[20];
@@ -23,15 +24,17 @@ int main()
 	if (sscanf(buf, "%s", input) == EOF)
 		return -1;
 
+	countdown = 3;
+
 	/* Initializes pseudo-random number generator using the current time. */
 	srand((unsigned)time(&t));
-	secret = rand();
+	secret = (rand() % 10) + 1;
 
 	/* DeMorgan's Law */
 	while (!(strcmp(input, "YES") && strcmp(input, "Yes") && strcmp(input, "yes") && strcmp(input, "Y") && strcmp(input, "y")))
 	{
 
-		printf("Please enter your guess: ");
+		printf("Please enter your guess from 1 - 10, you have %d tries remaining: ", countdown);
 		if (fgets(buf, 20, stdin) == NULL)
 			return -1;
 		if (buf[strlen(buf) - 1] == '\n')
@@ -42,15 +45,30 @@ int main()
 		if (sscanf(buf, "%d", &guess) == EOF)
 			return -1;
 
+		if (guess < 1 || guess > 10)
+		{
+			printf("Error, please enter a number from the correct range.\n");
+			continue;
+		}
+
+		--countdown;
+
 		if (guess == secret)
 		{
 			printf("You got it! The secret was %d\n", secret);
 			/* If user guessed correctly, randomize the new secret value. */
-			secret = rand();
+			secret = (rand() % 10) + 1;
+			countdown = 3;
 		}
 		else
 		{
 			printf("Aww you missed.\n");
+			if (countdown <= 0)
+			{
+				printf("The correct value was %d.\n", secret);
+				printf("Thank you very much for playing! Please try again some other time.\n");
+				break;
+			}
 		}
 
 		printf("Would you like to try again?\n");
